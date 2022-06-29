3a1189d1c5d51254472437b3b79b4b13ef25c161
Michelle A <michelleameral@gmail.com>
2022-06-29 12:36:40 -0400

    Week 1 Exercises

---
+++ battlegame.py
@@ -0,0 +1,50 @@
+wizard = "Wizard"
+elf = "Elf"
+human = "Human"
+wizard_hp = 70
+elf_hp = 100
+human_hp = 150
+wizard_damage = 150
+elf_damage = 100
+human_damage = 20
+dragon_hp = 300
+dragon_damage = 50
+
+while True:
+    print('1) Wizard\n 2) Elf\n 3) Human')
+    character = input("Choose your character: ")
+    if character == "1" or character.lower() == "wizard":
+        character = wizard
+        my_hp = wizard_hp
+        my_damage = wizard_damage
+        break
+    if character == "2" or character.lower() == "elf":
+        character = elf
+        my_hp = elf_hp
+        my_damage = elf_damage
+        break
+    if character == "3" or character.lower() == "human":
+        character = human
+        my_hp = human_hp
+        my_damage = human_damage
+        break
+    print("Unknown character")
+
+print('You have chosen the character: ' + character)
+print('Health: ' + str(my_hp))
+print('Damage: ' + str(my_damage), "\n")
+
+while True:
+    dragon_hp -= my_damage
+    print("The", character, "damaged the Dragon!")
+    print("The Dragon's hitpoints are now: ", dragon_hp, "\n")
+    if dragon_hp <= 0:
+        print("The Dragon has lost the battle")
+        break
+    my_hp -= dragon_damage
+    print("The Dragon strikes back at", character)
+    print("The", character + "'s", "hitpoints are now: ", my_hp, "\n")
+    if my_hp <= 0:
+        print("The", character, "has lost the battle")
+        break
+
---
+++ cc1.py
@@ -0,0 +1,22 @@
+# Turn this line into a comment
+
+cities = ["Seattle", "Tacoma", "Bellevue"]
+
+a = 1
+print(type(a))
+
+b = 10.2
+print(type(b))
+
+c = "This is a string."
+print(type(c))
+
+d = a + b
+print(d)
+
+
+
+
+
+
+
