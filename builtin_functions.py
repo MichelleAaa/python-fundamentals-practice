fb291ae498c8c928ca36f8d369dbe01a825bdc61
Michelle A <michelleameral@gmail.com>
2022-07-04 14:18:57 -0400

    loops

---
+++ builtin_functions.py
@@ -0,0 +1,13 @@
+print("Math Functions")
+
+abs_int = abs(-1) # provides absolute value. Changes negative to positive. - 1
+print(abs_int)
+
+int_to_float = float(100) #100.0
+print(int_to_float)
+
+print(int(1.23)) #    1
+print(max(1, 2, -5, 10, 0)) # will print the max value - 10
+print(min(1, 2, -5, 10, 0)) # will print out the min value - -5
+print(pow(2, 3)) # raises the number 2 to the power of 3 - 8
+print(round(51.6)) # will print 52
